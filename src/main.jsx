b1930e1242aa60a24b29054101b0ec3a4bfdae58
Dpungee <donald.punger@outlook.com>
2026-03-30 21:16:19 -0400

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import CrudeRush from './CrudeRush.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <CrudeRush />
+  </React.StrictMode>
+);
